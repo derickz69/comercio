b188f03453c15071eb6f07f6499e55457c0a58ac
Eduardo Derick <edduardodeerick123@gmail.com>
2023-06-06 07:42:49 -0300

    Início Controle de Versão

---
+++ src/main/java/com/itb/inf2am/comercio/model/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.inf2am.comercio.model;
+
+public class Produto {
+private Long id;
+private String nome;
+private String descricao;
+private String codigoBarras;
+private double preco;
+
+ 
+
+
+
+
+
+}
